5deeda07bc5819aba999d0e22e551090a79e0d06
Thành Tài <taint0812@gmail.com>
2023-02-19 11:48:47 +0700

    1

--- main.tf
+++ main.tf
@@ -10,16 +10,32 @@ terraform {
 provider "digitalocean" {
   token = var.do_token
 }
-
-resource "digitalocean_droplet" "web" {
-  image              = "ubuntu-18-04-x64"
-  name               = "tai-kafka"
-  region             = "sgp1"
-  size               = "s-1vcpu-2gb"
-  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
+resource "digitalocean_ssh_key" "default" {
+  name       = "DO Default SSH Key"
+  public_key = var.ssh_public_key
 }
 
-resource "digitalocean_ssh_key" "default" {
-  name = "tai-kafka"
-  public_key = file(".ssh/id_rsa.pub")
+resource "digitalocean_droplet" "example" {
+  image  = "ubuntu-20-04-x64"
+  name   = "kafka"
+  region = "sgp1"
+  size   = "s-1vcpu-2gb"
+  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = var.ssh_private_key
+    host        = self.ipv4_address
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p ~/.ssh",
+      "echo '${var.ssh_public_key}' >> ~/.ssh/authorized_keys",
+      "chmod 700 ~/.ssh",
+      "chmod 600 ~/.ssh/authorized_keys",
+      "service ssh restart",
+    ]
+  }
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,11 @@ variable "do_token" {
   type = string
   default = "dop_v1_63234fc4bc0d72389f9365aa8d3b530a718a6986e2ece96879613973c18fc75f"
 }
+
+variable "ssh_public_key" {
+  description = "Public key to connect to instance through SSH"
+}
+
+variable "ssh_private_key" {
+  description = "Private key to connect to instance through SSH"
+}
