4e355b1b4f572c355e614fea23a347eafaa86976
Thành Tài <taint0812@gmail.com>
2023-02-28 10:09:59 +0700

    Kafka | final

--- terraform.tf
+++ terraform.tf
@@ -20,7 +20,7 @@ resource "digitalocean_droplet" "example" {
     # sudo cat /etc/fstab >> /swapfile swap swap defaults 0 0
 
     # INSTALL DOCKER
-    curl -fsSL https://get.docker.com -o get-docker.sh
+    su -c "curl -sSL https://get.docker.com/ | sh"
     sudo sh get-docker.sh
 
     # # ADD DOCKER USER AS ROOT USER
